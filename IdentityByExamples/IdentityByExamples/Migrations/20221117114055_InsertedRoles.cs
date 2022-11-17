4de3e2d2b448df6a3081acadd1717a20ed164c0b
Vakaer <waqarmujeeb153@gmail.com>
2022-11-17 18:22:41 +0500

    initial Commit

---
+++ IdentityByExamples/IdentityByExamples/Migrations/20221117114055_InsertedRoles.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityByExamples.Migrations
+{
+    public partial class InsertedRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "36d4043e-63d4-4463-b7a0-64a3c18faf2e", "fc1a9ad9-fcb3-43f0-a55d-fddf360661c6", "Visitor", "VISITOR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "42984ff0-f4ce-4882-8e87-252678dbba71", "44e41e2c-4fb5-4017-b5e1-1cf0ae865631", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "36d4043e-63d4-4463-b7a0-64a3c18faf2e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "42984ff0-f4ce-4882-8e87-252678dbba71");
+        }
+    }
+}
